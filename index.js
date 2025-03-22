1bd69b8fe52d0730d76a18ee33437e090d9007f2
sabbir1257 <masabbir1257@gmail.com>
2025-03-22 13:06:41 +0600

    Simple Overview of React Query and load data

--- index.js
+++ index.js
@@ -41,13 +41,18 @@ async function run() {
     });
 
     // carts collection
+    app.get('/carts', async(req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result)
+    })
+
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
 
-    
+
   } catch (error) {
     console.error("MongoDB connection error:", error);
   }

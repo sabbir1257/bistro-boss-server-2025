0490c15ec078def1bf02c4cd248e4bfa90940321
sabbir1257 <masabbir1257@gmail.com>
2025-04-10 12:12:23 +0600

    verify token and axios interceptor

--- index.js
+++ index.js
@@ -43,12 +43,18 @@ async function run() {
 
     // middlewares
     const varifyToken = (req, res, next) => {
-      console.log("inside verify token", req.headers);
+      console.log("inside verify token", req.headers.authorization);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: "forbidden access" });
       }
-      const token = req.headers.authorization.split(' ')[1];
-      // next();
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
     };
 
     // ✅ API Routes
